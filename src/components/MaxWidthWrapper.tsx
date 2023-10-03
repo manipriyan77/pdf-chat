6a07d6579e0167a3e160743d3287121b842b88e9
manipriyan77 <manipriyangopalan@gmail.com>
2023-10-03 22:53:30 +0530

    completed the home page UI design

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,14 @@
+import { cn } from '@/lib/utils';
+import { ReactNode } from 'react';
+
+interface Props {
+  className?: string;
+  children: ReactNode;
+}
+const MaxWidthWrapper = ({ className, children }: Props) => {
+  return (
+    <div className={cn('mx-auto w- max-w-screen-xl px-2.5 md:px-20', className)}>{children}</div>
+  );
+};
+
+export default MaxWidthWrapper;
